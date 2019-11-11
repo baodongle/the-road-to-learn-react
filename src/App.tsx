8da92bd6a5c468b8078f44cd0017d3b2e2f35aae
baodongle <baodongle@gmail.com>
2019-11-11 17:48:44 +0700

    Introduction to JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React, {Component, ReactNode} from 'react';
 import './App.scss';
-import logo from './logo.svg';
 
 class App extends Component {
   render(): ReactNode {
+    var helloWorld = 'Welcome to the Road to learn React';
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <h2>{helloWorld}</h2>
       </div>
     );
   }
