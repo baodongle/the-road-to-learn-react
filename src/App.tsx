025a15db1cdf046469bd075877090a23bd3e5136
baodongle <baodongle@gmail.com>
2019-11-11 18:08:40 +0700

    Complex JavaScript in JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React, {Component, ReactNode} from 'react';
 import './App.scss';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    numComments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    numComments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render(): ReactNode {
-    const helloWorld = 'Welcome to the Road to learn React';
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        {list.map(function(item) {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.numComments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
